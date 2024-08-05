47b5696187940162da0e73a31f21cc28b1044d7b
Angkit Khadka <khadkaankit85@gmail.com>
2024-08-04 23:42:14 -0400

    334  added

---
+++ 334. Increasing Triplet Subsequence.js
@@ -0,0 +1,11 @@
+/*
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+*/
